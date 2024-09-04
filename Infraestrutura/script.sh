e4326f454db600844d7cca40fe8fc115237194f6
Foiac <kaio_cfs@hotmail.com>
2024-09-03 22:00:24 -0300

    update .sh

--- Infraestrutura/script.sh
+++ Infraestrutura/script.sh
@@ -27,17 +27,17 @@ az eventhubs namespace authorization-rule create --resource-group $rgName --name
 
 # Create Azure Key Vault
 az keyvault create --name $kvName --resource-group $rgName --location $region
-
-# Storage Connection String on Key Vault
-connectionString=$(az eventhubs namespace authorization-rule keys list --resource-group $rgName --namespace-name $namespaceName --name $policieName --query primaryConnectionString --output tsv)
 az role assignment create --assignee $assigneeId --role "Key Vault Administrator" --scope /subscriptions/$subsId/resourceGroups/$rgName/providers/Microsoft.KeyVault/vaults/$kvName
-az keyvault secret set --vault-name $kvName --name $secretName --value $connectionString
 
-# Create Storage Account on Azure and create container on ADLS
-az storage account create --name $storageAcName --resource-group $rgName --location $region --sku Standard_LRS --kind StorageV2 --hierarchical-namespace true
-az storage container create --name $containerName --account-name $storageAcName --auth-mode login
+# # Storage Connection String on Key Vault
+# connectionString=$(az eventhubs namespace authorization-rule keys list --resource-group $rgName --namespace-name $namespaceName --name $policieName --query primaryConnectionString --output tsv)
+# az keyvault secret set --vault-name $kvName --name $secretName --value $connectionString
 
-# Create Paths to Delta Tables
-az storage fs directory create --account-name $storageAcName --file-system $containerName --name Bronze
-az storage fs directory create --account-name $storageAcName --file-system $containerName --name Silver
-az storage fs directory create --account-name $storageAcName --file-system $containerName --name Gold
+# # Create Storage Account on Azure and create container on ADLS
+# az storage account create --name $storageAcName --resource-group $rgName --location $region --sku Standard_LRS --kind StorageV2 --hierarchical-namespace true
+# az storage container create --name $containerName --account-name $storageAcName --auth-mode login
+
+# # Create Paths to Delta Tables
+# az storage fs directory create --account-name $storageAcName --file-system $containerName --name Bronze
+# az storage fs directory create --account-name $storageAcName --file-system $containerName --name Silver
+# az storage fs directory create --account-name $storageAcName --file-system $containerName --name Gold
