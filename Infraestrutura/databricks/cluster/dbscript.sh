cbb9e035a661ca2384920c45bf7ca0c7bc7c8300
Foiac <kaio_cfs@hotmail.com>
2024-09-09 23:13:28 -0300

    update project

--- Infraestrutura/databricks/cluster/dbscript.sh
+++ Infraestrutura/databricks/cluster/dbscript.sh
@@ -2,3 +2,5 @@ echo "Para continuar digite seu host e token de acesso do databricks"
 databricks configure --token
 
 databricks clusters create --json-file cluster_config.json
+
+databricks repos create --url https://github.com/Foiac/MobileFraudDetectSolution.git --provider gitHub
