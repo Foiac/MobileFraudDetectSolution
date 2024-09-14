61daa6bb73bd24aaeb731ece2cee1b4ed55e070e
Caio França <37381656+Foiac@users.noreply.github.com>
2024-09-14 11:38:48 -0300

    Update dbscript.sh

--- Infraestrutura/databricks/cluster/dbscript.sh
+++ Infraestrutura/databricks/cluster/dbscript.sh
@@ -3,7 +3,7 @@ SCOPE_NAME="secret-fraud"
 echo "Para continuar digite seu host e token de acesso do databricks"
 databricks configure --token
 
-databricks clusters create --json-file MobileFraudDetectSolution/Infraestrutura/databricks/cluster/dbscript.sh/cluster_config.json
+databricks clusters create --json-file MobileFraudDetectSolution/Infraestrutura/databricks/cluster/cluster_config.json
 databricks secrets create-scope --scope $SCOPE_NAME
 databricks secrets put --scope $SCOPE_NAME --key SECRET_CONNECTION_STRING
 databricks secrets put --scope $SCOPE_NAME --key SECRET_AZURE_KEY
